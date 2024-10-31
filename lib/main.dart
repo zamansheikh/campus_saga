78eaa514b0474446e7db541619fac4538c994b23
Zaman Sheikh <zaman6545@gmail.com>
2024-10-31 17:04:28 +0600

    graddle fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,3 @@
-// import 'package:campus_saga/app/data/providers/auth/auth_controller.dart';
-// import 'package:campus_saga/app/routes/app_pages.dart';
-// import 'package:campus_saga/presentation/pages/splash/splash_screen.dart';
-// import 'package:flutter/material.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:get/get.dart';
-// import 'firebase_options.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//   Get.put(AuthController());
-//   runApp(
-//     GetMaterialApp(
-//       title: "Application",
-//       initialRoute: AppPages.INITIAL,
-//       getPages: AppPages.routes,
-//       debugShowCheckedModeBanner: false,
-//     ),
-//   );
-// }
-
 // lib/main.dart
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/injection_container.dart' as di;
