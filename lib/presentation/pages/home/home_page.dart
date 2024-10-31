788340da93c95571da3b41bb4724d5ffc31262b0
Zaman Sheikh <zaman6545@gmail.com>
2024-10-31 21:59:16 +0600

    signup implemented

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,18 +1,40 @@
-// lib/pages/home/home_page.dart
-
+import 'package:campus_saga/presentation/bloc/auth/auth_bloc.dart';
+import 'package:campus_saga/presentation/bloc/auth/auth_event.dart';
 import 'package:campus_saga/presentation/pages/widgets/post_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../bloc/auth/auth_state.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
-      body: ListView.builder(
-        itemCount: 10, // Placeholder count for posts
-        itemBuilder: (context, index) {
-          return PostCard(postId: 'post_$index'); // Dummy postId
+      appBar: AppBar(
+        title: const Text("Home"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              // Dispatch the logout event
+              BlocProvider.of<AuthBloc>(context).add(SignOutEvent());
+            },
+          ),
+        ],
+      ),
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthUnauthenticated) {
+            // Navigate to the login page or any other appropriate action
+            Navigator.of(context).pushReplacementNamed('/login');
+          }
         },
+        child: ListView.builder(
+          itemCount: 10, // Placeholder count for posts
+          itemBuilder: (context, index) {
+            return PostCard(postId: 'post_$index'); // Dummy postId
+          },
+        ),
       ),
     );
   }
