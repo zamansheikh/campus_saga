8aae5cc16dc1f145636bab4c1fdcd28995d5d5f9
Zaman Sheikh <66895637+zamansheikh@users.noreply.github.com>
2025-05-30 12:54:29 +0600

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,7 +3,6 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
-
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
 /// Example:
